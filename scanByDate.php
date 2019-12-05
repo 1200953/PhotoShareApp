30b3f0d750733ef1c209dc8c1c33b04305a77930
Andy Xie <kxie0003@student.monash.edu>
2019-12-05 23:08:53 +1100

    根据时间返回文件名

---
+++ scanByDate.php
@@ -0,0 +1,78 @@
+<?php
+require './vendor/autoload.php';
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
+$IAM_KEY = 'AKIAVLSEBUAJXLPCLWGM';
+$IAM_SECRET = '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';
+$yr = $_GET["yr"];
+$mth = $_GET["mth"];
+$day = $_GET["day"];
+
+
+$sdk = new Aws\Sdk([
+    'region'   => 'cn-north-1',
+    'version'  => 'latest'
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+$marshaler = new Marshaler();
+
+try{
+    $client = \Aws\DynamoDb\DynamoDbClient::factory(
+        array(
+            'credentials' => array(
+                'key' => $IAM_KEY,
+                'secret' => $IAM_SECRET
+            ),
+        'version' => 'latest',
+        'region' => 'cn-north-1',
+        // 'endpoint' => 'http://localhost:8000'
+    ));
+    } catch (Exception $e){
+        die("Error: " . $e->getMessage());
+    }
+
+    
+
+
+    $eav = $marshaler->marshalJson('
+    {
+        ":date" : "' .$day .'/'. $mth .'/' . $yr.'"' . '
+    }
+    ');
+
+    $params = [
+        'TableName' => 'hire2020-hire-xiekaihan-11291129',
+        'FilterExpression' => '#dt = :date',
+        'ExpressionAttributeNames'=> [ '#dt' => 'date' ],
+        'ExpressionAttributeValues'=> $eav
+
+        
+    ];
+
+    try {
+        $clist = array();
+        $result = $dynamodb->scan($params);
+
+            echo "Query succeeded.\n";
+
+        
+        
+        foreach ($result['Items'] as $user) {
+            echo $data = $marshaler->unmarshalValue($user['ID']);
+            array_push( $clist, $data);
+        }
+             
+
+        $tmpJson = json_encode($clist);
+
+        echo $tmpJson;
+          
+        // echo $response;
+        }catch (DynamoDbException $e){
+            die('Error:' . $e->getMessage());
+        }
+
+?>
